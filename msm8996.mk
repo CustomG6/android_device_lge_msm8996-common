8910f772f6c8dd74ec4d3ce0b58dbde763e85947
Michael Bardoň <wolfovelp@gmail.com>
2021-01-14 20:22:40 +0100

    Add Parts from https://github.com/LG-G6-DEV/android_device_lge_msm8996-common/tree/lineage-18.0/parts

--- msm8996.mk
+++ msm8996.mk
@@ -234,6 +234,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# LGE settings
+PRODUCT_PACKAGES += \
+    LGESettings
+
 # LiveDisplay
 # PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
